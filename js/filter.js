945ced65dfbebb36c5f47930b94fa6dc1298c734
Noot <nathan.boisleux@edu.devinci.fr>
2024-02-11 19:34:17 +0100

    adding card description page

--- js/filter.js
+++ js/filter.js
@@ -1,39 +1,18 @@
 document.querySelectorAll(".maison-btn").forEach(btn => {
-    btn.addEventListener("click", function() {
-        let maison = this.getAttribute("data-maison")
+    btn.addEventListener("click", () =>  {
+        let maison = btn.getAttribute("data-maison")
         filterCartes(maison)
         updateCouleurs(maison)
-        // changeBackground(maison)
+        
     })
 })
 
 document.getElementById("reset").addEventListener("click", () => {
     filterCartes() // sans argument pour tout afficher
     updateCouleurs() // reinitialiser les couleurs
-    // changeBackground()
+    
 })
 
-// function changeBackground(maison) {
-//     const maisonsContainer = document.getElementById("maisons") 
-//     switch(maison) {
-//         case "Gryffondor":
-//             maisonsContainer.style.backgroundColor = "#740001"
-//             break
-//         case "Serpentard":
-//             maisonsContainer.style.backgroundColor = "#1a472a"
-//             break
-//         case "Serdaigle":
-//             maisonsContainer.style.backgroundColor = "#222f5b"
-//             break
-//         case "Poufsouffle":
-//             maisonsContainer.style.backgroundColor = "#ffdb00"
-//             break
-//         default:
-//             // Vous pouvez définir une couleur par défaut si nécessaire
-//             maisonsContainer.style.backgroundColor = "#E4EBF5"
-//             break
-//     }
-// }
 
 function filterCartes(maison) {
     document.querySelectorAll(".hp-card").forEach(carte => {
