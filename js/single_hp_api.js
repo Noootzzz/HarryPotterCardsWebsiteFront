c8a1889907478cc67ee59cb087f1a27ef73ed371
Noot <nathan.boisleux@edu.devinci.fr>
2024-05-03 12:31:51 +0200

    adding fetchHouse

--- js/single_hp_api.js
+++ js/single_hp_api.js
@@ -1,15 +1,17 @@
-//fonction fetch qui recupere les informations 
+//fonction fetch qui recupere les informations
 function fetchCharacter() {
-    let url = window.location.search
-    let slug = new URLSearchParams(url).get('slug')
-    return fetch('https://hp-api.lainocs.fr/characters/' + slug)
-    .then((response) => response.json())
+  let url = window.location.search;
+  let slug = new URLSearchParams(url).get("slug");
+  return fetch("https://hp-api.lainocs.fr/characters/" + slug).then(
+    (response) => response.json()
+  );
 }
 
 //fonction asynchrone pour afficher
 async function displayCharacter() {
-    const data = await fetchCharacter()
-    document.querySelector('#card-view').innerHTML = `
+  const data = await fetchCharacter();
+
+  document.querySelector("#card-view").innerHTML = `
         <h2>${data.name}</h2>
         <img src="${data.image}" alt="${data.name}">
         <p>House : ${data.house}</p>
@@ -17,7 +19,32 @@ async function displayCharacter() {
         <p>Role : ${data.role}</p>
         <p>Patronus : ${data.patronus}</p>
         <p>Blood : ${data.blood}</p>
-        <a href="details.html">Back</a>`
+        <a href="details.html">Back</a>`;
+  console.log(data.house);
 }
 //appel de la fonction
-displayCharacter()
+displayCharacter();
+
+async function fetchHouse() {
+  const data = await fetchCharacter();
+  const house = data.house;
+
+  // envoi de la maison à l'URL avec la méthode POST
+  fetch("http://192.168.69.234:3000/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ house: house }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Réponse de l'API :", data);
+    })
+    .catch((error) => {
+      console.error("Erreur lors de l'envoi de la maison :", error);
+    });
+}
+
+// Appel de la fonction fetchHouse()
+fetchHouse();
