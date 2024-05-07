f7fd4b5a2dc731ec6e9cb79c82e9dde7e27f3c22
Noot <nathan.boisleux@edu.devinci.fr>
2024-05-07 15:25:52 +0200

    adding register

--- js/register.js
+++ js/register.js
@@ -1,1 +1,26 @@
+const formulaire = document.getElementById("reg-form");
 
+formulaire.addEventListener("submit", async (event) => {
+  event.preventDefault(); //eviter que la page se recharger quand on envoie
+  const name = document.getElementById("reg-name").value;
+  const email = document.getElementById("reg-email").value;
+  const password = document.getElementById("reg-psw").value;
+
+  const response = await fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, password }),
+  });
+
+  const data = await response.json();
+
+  if (data) {
+    window.location.href = "login.html";
+    alert("Inscritpion réussi, maintenant connectez-vous.");
+  }
+  if (!data) {
+    alert("Erreur d'inscription.");
+  }
+});
